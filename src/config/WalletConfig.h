3c8ec2ad646e333b117238fc5520e672ef0eb130
satorigold <68770558+satorigold@users.noreply.github.com>
2020-08-11 16:39:06 +0800

    Update WalletConfig.h

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -14,7 +14,7 @@ namespace WalletConfig
     const std::string_view addressPrefix = "Xau";
 
     /* Your coins 'Ticker', e.g. Monero = XMR, Bitcoin = BTC */
-    const std::string ticker = "SXAU";
+    const std::string ticker = "SCX";
 
     /* The filename to output the CSV to in save_csv */
     const std::string csvFilename = "transactions.csv";
